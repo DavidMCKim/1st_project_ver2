2785e8640f167b89721b4f81dfad317f6b5e05a5
MCDAVID <kimminchang96@gmail.com>
2021-08-23 21:35:14 +0900

    1st_project_web_part_ver.2

--- js/button.js
+++ js/button.js
@@ -1,19 +1,12 @@
 /* button.js */
 
 $(document).ready(function(){
-    $('#total').on('click', function(){
-        $("#div1").load("Top3_응급실반경_folium(노인교통사고사망자다발지역포함)(결과).html");
+    $('#analysisPNG').mouseover(function(){
+        $("#analysisPNG").hide();
+        $("#analysisText").show();
     });
-    $('#ch').on('click', function(){
-        $(".container-fluid p-0").css("margin-left","500px");
-        $("#div1").load("충남_병원_반경_사망자다발지역_최종.html");
-    });
-    $('#jn').on('click', function(){
-        $(".container-fluid p-0").css("margin-left","500px");
-        $("#div1").load("전남_병원_반경_사망자다발지역_최종.html");
-    });
-    $('#jj').on('click', function(){
-        $(".container-fluid p-0").css("margin-left","500px");
-        $("#div1").load("제주_병원_반경_사망자다발지역_최종.html");
+    $("#analysisPNG").mouseleave(function(){
+       $("#analysisPNG").show();
+       $("#analysisText").hide();
     });
 });
